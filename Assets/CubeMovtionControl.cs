376d54115fb7db8ada965786041671a8e678449a
Bai <a0960995331@gmail.com>
2026-04-01 17:14:16 +0800

    新增了一個方塊運動程式

---
+++ Assets/CubeMovtionControl.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeMovtionControl : MonoBehaviour
+{
+    public float velocity;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.A)){transform.position += Vector3.left * velocity * Time.deltaTime;}
+	    if(Input.GetKey(KeyCode.D)){transform.position += Vector3.right * velocity * Time.deltaTime;}
+	    if(Input.GetKey(KeyCode.W)){transform.position += Vector3.up * velocity * Time.deltaTime;}
+	    if(Input.GetKey(KeyCode.S)){transform.position += Vector3.down * velocity * Time.deltaTime;}	 
+    }
+}
